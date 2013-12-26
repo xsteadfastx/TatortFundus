75ae575fb987f632455c1030281cbc6d569fa67d
Marvin Steadfast <xsteadfastx@gmail.com>
2013-12-26 02:11:39 +0100

    removed mechanize and fixed the umlaut problem with requests and BeautifulSoup

--- TatortFundus.py
+++ TatortFundus.py
@@ -1,29 +1,31 @@
 import re
-from mechanize import Browser
+import requests
 from bs4 import BeautifulSoup
 
 
 class Episode(object):
     def __init__(self, episoden_name):
         self.episoden_name = episoden_name
+        self.episoden_name = self.episoden_name.decode('utf-8')
+        self.URL = "http://www.tatort-fundus.de/web/folgen/alpha.html"
         self.full_site = self._browse()
         self.soup = BeautifulSoup(self.full_site)
 
+    def _get_link(self, name, URL):
+        r = requests.get(URL)
+        soup = BeautifulSoup(r.text)
+        links = soup.findAll('div', 'folgen_suche')
+        for i in links:
+            if name in i.text:
+                return i.a['href']
+            else:
+                pass
+
     def _browse(self):
-        br = Browser()
-        br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686;\
-            en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9\
-            Firefox/3.0.1')]
-        br.set_handle_robots(False)
-        br.open("http://www.tatort-fundus.de/web/folgen/alpha.html")
-        episoden_name = self.episoden_name
-
-        br.find_link(text=episoden_name[0])
-        br.follow_link(text=episoden_name[0])
-
-        br.find_link(text=episoden_name)
-        response = br.follow_link(text=episoden_name)
-        return response.read()
+        start_letter_page = self._get_link(self.episoden_name[0], self.URL)
+        letter_page = self._get_link(self.episoden_name, start_letter_page)
+        r = requests.get(letter_page)
+        return r.text
 
     def _table_soup(self, info_item):
         soup = self.soup
@@ -37,6 +39,7 @@ class Episode(object):
             for i in inhalt_extract:
                 if i.startswith(info_item):
                     item_position = inhalt_extract.index(i)
+
                 else:
                     pass
 
